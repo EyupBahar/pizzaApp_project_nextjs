9918ba8da3a903adbddf390a1626aacbd419d71f
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-05-22 23:57:50 +0300

    add index.js updates

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    tittle: "A First Mettup",
+    image: "",
+    address: "Some address 5, 12345 San Francisco",
+  },
+];
+
 function HomePage() {
   return;
 }
