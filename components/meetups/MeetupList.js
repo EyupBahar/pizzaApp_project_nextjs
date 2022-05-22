0754f04e1021a61c8bbcb33151eefa977c30d7d3
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-05-22 23:53:30 +0300

    add meetupList.js

---
+++ components/meetups/MeetupList.js
@@ -0,0 +1,18 @@
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
+
+function MeetupList(props) {
+  return (
+    <ul className={classes.light}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+        />
+      ))}
+    </ul>
+  );
+}
