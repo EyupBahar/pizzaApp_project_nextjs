5267ba317513f225eb995882672f1dd320cdfdcf
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-05-28 00:29:17 +0300

    meetup detail updates

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,12 +1,12 @@
 import classes from "./MeetupDetail.module.css";
 
-function MeetupDetail() {
-  <Fragment>
+function MeetupDetail(props) {
+  <section className={classes.detail}>
     <img src={props.image} alt={props.title} />
     <h1>{props.title}</h1>
     <address>{props.address}</address>
     <p>{props.description}</p>
-  </Fragment>;
+  </section>;
 }
 
 export default MeetupDetail;
