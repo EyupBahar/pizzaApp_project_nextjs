03bbd4cb114f747bc06ca916c2d835bb66061250
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-05-28 23:20:47 +0300

    add folder changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import Head from "next/head";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
-import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,19 @@
-import MeetupList from "../components/meetups/MeetupList";
+import Head from "next/head";
+import Image from "next/image";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+import styles from "../styles/Home.module.css";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    tittle: "A First Mettup",
-    image:
-      "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcT2DaqEuWx43Jr3F4ok2JePyDMZDIgwarrMiDC5uyk_SvHs1IB6nwQzjixZxdRX",
-    address: "Some address 5, İstanbul",
-  },
-  {
-    id: "m2",
-    tittle: "A Second Mettup",
-    image:
-      "https://iasbh.tmgrup.com.tr/bbe9c0/650/344/0/0/887/467?u=https://isbh.tmgrup.com.tr/sbh/2019/09/18/bursada-gezilecek-yerler-bursada-gorulmesi-gereken-10-guzel-yer-1568810140805.jpg",
-    address: "Some address 5, 12345 Bursa",
-  },
-];
-
-function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured/>
+      <PizzaList/>
+    </div>
+  );
 }
-
-/* export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
-
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
-} */
-
-export async function getStaticProps() {
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-    revalidate: 1,
-  };
-}
-
-export default HomePage;
